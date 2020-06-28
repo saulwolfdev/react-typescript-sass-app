dab920e2034d4e356cddb844cce0038b18f5b6f0
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2020-06-28 02:40:25 -0300

    build layout

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,23 +1,23 @@
 
 import * as React from "react";
-import {Fragment} from "react";
+import { Fragment } from "react";
 import { Mocks } from "../../Mocks";
 import { IItemsTaskNames } from "../../Entidades/IItemsTaskNames";
 import List from "../List/List";
 import styles from "./Main.module.scss";
 export interface MainProps {
-	
+
 }
- 
+
 export interface MainState {
-	itemsCards:Array<IItemsTaskNames>
+	itemsCards: Array<IItemsTaskNames>
 }
- 
+
 class Main extends React.Component<MainProps, MainState> {
 	public constructor(props: MainProps) {
 		super(props);
 		this.state = {
-			itemsCards:[],
+			itemsCards: [],
 		};
 	}
 
@@ -29,14 +29,23 @@ class Main extends React.Component<MainProps, MainState> {
 		});
 	}
 
-	public render() { 
+	public render() {
 		return (
 			<Fragment>
-				<List itemsCards={this.state.itemsCards}/>
-				
+				{/* <List itemsCards={this.state.itemsCards}/> */}
+				<div className={styles.Container}>
+					<div className={styles.Container_Header}><h1>ITEM</h1></div>
+					<div className={styles.Container_Enter}><h1>ENTERPRICE</h1></div>
+					<div className={styles.Container_Enter_Image}><h1>IMAGE</h1></div>
+					{/* <div><h1>ITEM</h1></div>
+					<div><h1>ITEM</h1></div>
+					<div><h1>ITEM</h1></div>
+					<div><h1>ITEM</h1></div> */}
+				</div>
+
 			</Fragment>
 		);
 	}
 }
- 
+
 export default Main;
